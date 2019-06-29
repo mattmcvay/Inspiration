1caed69442faedb7025a20075a291e0b14f1e38d
mattmcvay <matt.mcvay@gmx.com>
2019-06-29 19:17:56 -0400

    initial commit

---
+++ javascript.js
@@ -0,0 +1,47 @@
+// *** Code from w3schools.com ***
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1} 
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none"; 
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block"; 
+  dots[slideIndex-1].className += " active";
+}
+
+//------------------------------------------------------------------------------------
+
+// *** Custom javaScript functions ***
+function colorChangeBlue(){
+  document.querySelector("html").style.background = "cadetblue"
+  }
+
+  function colorChangeGrey(){
+    document.querySelector("html").style.background = "Grey";
+    }
+
+
+    
+   
+
+
+
